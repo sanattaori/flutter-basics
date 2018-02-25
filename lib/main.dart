d4cc36e7419dfbfa3f6aa2786ceb0f39d9fce2ba
sanattaori <sanattaori3@gmail.com>
2018-02-25 19:08:02 +0530

    StatelessWidget Cards

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,37 @@ class MyScaffold extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(title: new Text("First Flutter"),),
+      appBar: new AppBar(title: new Text("First Flutter")),
+      backgroundColor: Colors.yellowAccent,
+      body: new Container(
+        child: new Column(
+          children: <Widget>[
+            new MyCard(),
+            new MyCard(),
+          ],
+        ),
+      ),
     );
   }
+}
 
+class MyCard extends StatelessWidget{
+  MyCard({this.title, this.icon});
+
+  final Widget title;
+  final Widget icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      child: new Card(
+        child: new Column(
+          children: <Widget>[
+            new Text("Test1 "),
+            new Icon(Icons.favorite)
+          ],
+        ),
+      ),
+    );
+  }
 }
