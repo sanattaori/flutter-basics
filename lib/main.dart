813667d1d3620e2eb531ee2aa978c61a61980d03
sanattaori <sanattaori3@gmail.com>
2018-02-25 18:58:32 +0530

    First Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,16 @@ import 'package:flutter/material.dart';
 void main() {
   //runApp widget
   runApp(new MaterialApp(
-    home: new Text("Cool My First Flutter App"),
+    home: new MyScaffold(),
   ));
+}
+
+class MyScaffold extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("First Flutter"),),
+    );
+  }
+
 }
